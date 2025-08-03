f8ab6e84303d820c93d447ddc25fe97ff11eba47
turbou <turbou@i.softbank.jp>
2025-08-03 10:45:46 +0900

    import.

---
+++ src/main/java/com/contrastsecurity/vulnstatusmanagetool/preference/PreferenceConstants.java
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ * Copyright (c) 2020 Contrast Security Japan G.K.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
+package com.contrastsecurity.vulnstatusmanagetool.preference;
+
+public class PreferenceConstants {
+    public static final String CONTRAST_URL = new String("com.contrastsecurity.vulnstatusmanagetool.contrast_url"); //$NON-NLS-1$
+    public static final String USERNAME = new String("com.contrastsecurity.vulnstatusmanagetool.username"); //$NON-NLS-1$
+    public static final String SERVICE_KEY = new String("com.contrastsecurity.vulnstatusmanagetool.service_key"); //$NON-NLS-1$
+    public static final String API_KEY = new String("com.contrastsecurity.vulnstatusmanagetool.api_key"); //$NON-NLS-1$
+    public static final String ORG_ID = new String("com.contrastsecurity.vulnstatusmanagetool.org_id"); //$NON-NLS-1$
+    public static final String IS_SUPERADMIN = new String("com.contrastsecurity.vulnstatusmanagetool.is_superadmin"); //$NON-NLS-1$
+    public static final String IS_CREATEGROUP = new String("com.contrastsecurity.vulnstatusmanagetool.is_creategroup"); //$NON-NLS-1$
+    public static final String GROUP_NAME = new String("com.contrastsecurity.vulnstatusmanagetool.group_name"); //$NON-NLS-1$
+    public static final String TARGET_ORGS = new String("com.contrastsecurity.vulnstatusmanagetool.target_orgs"); //$NON-NLS-1$
+
+    public static final String PROXY_YUKO = new String("com.contrastsecurity.vulnstatusmanagetool.proxy_yuko"); //$NON-NLS-1$
+    public static final String PROXY_HOST = new String("com.contrastsecurity.vulnstatusmanagetool.proxy_host"); //$NON-NLS-1$
+    public static final String PROXY_PORT = new String("com.contrastsecurity.vulnstatusmanagetool.proxy_port"); //$NON-NLS-1$
+    public static final String PROXY_AUTH = new String("com.contrastsecurity.vulnstatusmanagetool.proxy_auth"); //$NON-NLS-1$
+    public static final String PROXY_USER = new String("com.contrastsecurity.vulnstatusmanagetool.proxy_user"); //$NON-NLS-1$
+    public static final String PROXY_PASS = new String("com.contrastsecurity.vulnstatusmanagetool.proxy_pass"); //$NON-NLS-1$
+    public static final String PROXY_TMP_USER = new String("com.contrastsecurity.vulnstatusmanagetool.proxy_tmp_user"); //$NON-NLS-1$
+    public static final String PROXY_TMP_PASS = new String("com.contrastsecurity.vulnstatusmanagetool.proxy_tmp_pass"); //$NON-NLS-1$
+    public static final String IGNORE_SSLCERT_CHECK = new String("com.contrastsecurity.vulnstatusmanagetool.ignore_sslcert_check"); //$NON-NLS-1$
+    public static final String CONNECTION_TIMEOUT = new String("com.contrastsecurity.vulnstatusmanagetool.connection_timeout"); //$NON-NLS-1$
+    public static final String SOCKET_TIMEOUT = new String("com.contrastsecurity.vulnstatusmanagetool.socket_timeout"); //$NON-NLS-1$
+
+    public static final String DETECT_CHOICE = new String("com.contrastsecurity.vulnstatusmanagetool.detect_choice"); //$NON-NLS-1$
+    public static final String TERM_START_MONTH = new String("com.contrastsecurity.vulnstatusmanagetool.term_start_month"); //$NON-NLS-1$
+    public static final String START_WEEKDAY = new String("com.contrastsecurity.vulnstatusmanagetool.start_weekday"); //$NON-NLS-1$
+    public static final String AUDITLOG_CREATED_DATE_FILTER = new String("com.contrastsecurity.vulnstatusmanagetool.auditlog_created_date_filter"); //$NON-NLS-1$
+
+    public static final String OPENED_MAIN_TAB_IDX = new String("com.contrastsecurity.vulnstatusmanagetool.opened_main_tab_idx"); //$NON-NLS-1$
+    public static final String OPENED_SUB_TAB_IDX = new String("com.contrastsecurity.vulnstatusmanagetool.opened_sub_tab_idx"); //$NON-NLS-1$
+    public static final String MEM_WIDTH = new String("com.contrastsecurity.vulnstatusmanagetool.mem_width"); //$NON-NLS-1$
+    public static final String MEM_HEIGHT = new String("com.contrastsecurity.vulnstatusmanagetool.mem_height"); //$NON-NLS-1$
+}
